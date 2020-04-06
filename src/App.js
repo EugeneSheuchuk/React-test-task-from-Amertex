0b0c60a468b4679e3487c743dbb73448f7455d6e
Yauheni Sheuchuk <shevchuked@tut.by>
2020-04-06 15:01:24 +0300

    init: start project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './App.module.css';
+
+function App() {
+  return (
+    <div className={style.appContainer}>
+      hello
+    </div>
+  );
+}
+
+export default App;
