881041c8f59f1431a6cc4c1f6499ebc510205192
Yauheni Sheuchuk <shevchuked@tut.by>
2020-04-06 18:06:44 +0300

    refactor: DNS - function component

--- src/components/DNS/DNS.jsx
+++ src/components/DNS/DNS.jsx
@@ -4,48 +4,42 @@ import PropTypes from "prop-types";
 import RadioInput from '../RadioInput/RadioInput';
 import Field from "../Field/Field";
 
-class DNS extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+const DNS = (props) => {
+    const {key} = props;
+    return (
+        <React.Fragment>
+            <div className={style.choose}>
+                <div className={style.item}>
+                    <RadioInput name={'automatically_DNS_server_address'}/>
 
-    render() {
-        const {key} = this.props;
-        return (
-            <React.Fragment>
-                <div className={style.choose}>
-                    <div className={style.item}>
-                        <RadioInput name={'automatically_DNS_server_address'}/>
-
-                    </div>
-                    <div className={style.item}>
-                        <label htmlFor="">
-                            Obtain DNS server address automatically
-                        </label>
-                    </div>
                 </div>
-                <div className={style.choose}>
-                    <div className={style.item}>
-                        <RadioInput name={'use_DNS_server_address'}/>
-                    </div>
-                    <div className={style.item}>
-                        <label htmlFor="">
-                            Use the following DNS server address:
-                        </label>
-                    </div>
+                <div className={style.item}>
+                    <label htmlFor="">
+                        Obtain DNS server address automatically
+                    </label>
                 </div>
-                <Field fieldName={'Preferred DNS server:'} id={`P_DNS_server_${key}`}
-                       action={() => {
-                       }}
-                       required={true}/>
-                <Field fieldName={'Alternative DNS server:'} id={`A_DNS_server_${key}`}
-                       action={() => {
-                       }}
-                       required={false}/>
-            </React.Fragment>
-        );
-    }
-}
+            </div>
+            <div className={style.choose}>
+                <div className={style.item}>
+                    <RadioInput name={'use_DNS_server_address'}/>
+                </div>
+                <div className={style.item}>
+                    <label htmlFor="">
+                        Use the following DNS server address:
+                    </label>
+                </div>
+            </div>
+            <Field fieldName={'Preferred DNS server:'} id={`P_DNS_server_${key}`}
+                   action={() => {
+                   }}
+                   required={true}/>
+            <Field fieldName={'Alternative DNS server:'} id={`A_DNS_server_${key}`}
+                   action={() => {
+                   }}
+                   required={false}/>
+        </React.Fragment>
+    );
+};
 
 DNS.propTypes = {
     key: PropTypes.string.isRequired,
--- src/components/Network/Network.jsx
+++ src/components/Network/Network.jsx
@@ -14,6 +14,7 @@ const Network = () => {
                 </div>
                 <div>
                     <p>Wireless Settings</p>
+
                 </div>
             </div>
             <div className={style.footer}>
